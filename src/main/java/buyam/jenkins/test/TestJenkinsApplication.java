aed1b3ba992b3e77ca6477e1fca1b4cfed9b95b1
Truong Cong Ly <truongcongly139@gmail.com>
2024-11-12 17:26:18 +0700

    first commit

---
+++ src/main/java/buyam/jenkins/test/TestJenkinsApplication.java
@@ -0,0 +1,13 @@
+package buyam.jenkins.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestJenkinsApplication.class, args);
+	}
+
+}
---
+++ src/main/java/buyam/jenkins/test/controller/HelloController.java
@@ -0,0 +1,15 @@
+package buyam.jenkins.test.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String getHello(){
+        return  "Hello spring boot";
+    }
+
+
+
+}
